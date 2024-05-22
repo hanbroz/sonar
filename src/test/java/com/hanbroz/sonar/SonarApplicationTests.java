58ac9e49e2982c6b4bd8f200d21bdcb199f56c67
hanbroz <hanbroz@naver.com>
2024-05-22 21:52:03 +0900

    fdfd

---
+++ src/test/java/com/hanbroz/sonar/SonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbroz.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
